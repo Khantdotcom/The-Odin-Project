0580ca7685d1e0b9686fdb38fce93a9864b51c43
Khantdotcom <kokhanthtay@gmail.com>
2024-09-26 17:46:38 +0630

    Reload logic

--- Resturant/src/index.js
+++ Resturant/src/index.js
@@ -1,3 +1,9 @@
-console.log("GOOD VIBES");
-const content = document.createElement("div").innerHTML;
-content += ""
+import { Home } from "./modules";
+const content_display = document.getElementById("content");
+function InitialReload() {
+    content_display.removeChild
+    const content = document.createElement("div");
+    content.innerHTML += "Set up an HTML skeleton inside of src/template.html. Inside the body, add a <header> element that contains a <nav> with buttons (not links!) for different “tabs” (for example buttons for “Home”, “Menu” or “About” etc). Below the <header>, add a single <div id";
+};
+const dispaly = new Home(content_display);
+
---
+++ Resturant/src/modules.js
@@ -0,0 +1,15 @@
+export function Home(locaiton_) {
+    const text = document.createElement("h1");
+    const button_ = document.createElement("button");
+    location_.appendChild("text");
+    location_.appendChild(button_);
+};
+
+
+export function Menu(locaiton_){
+
+};
+
+export function AboutUs(locaiton_){
+
+};
