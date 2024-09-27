829d813ae9bcabd559ce551f0850db8b1ddbe8af
Khantdotcom <kokhanthtay@gmail.com>
2024-09-27 20:50:56 +0630

    Innitial Commit

--- Curio To-know-list App/src/index.js
+++ Curio To-know-list App/src/index.js
@@ -6,44 +6,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const topic = document.getElementById("topic-name");
 
     //data structure
-    const contents = {"Topic":{
-        "topic-name" : "gg",
-        "curio-list" : [{"curio": {"name": "khant", "priority": "urgent", "date": something}}]};
-
-    function New_book () {
-            this.author = author.value;
-            this.title = title.value;
-            this.pages = pages.value;
-        }
-
-    function AddBook(){
-        all_books.push();
-        }
-
-    function TopicDisplay(){
-        new_area = document.createElement("div");
-        new_area.classList.add("new_area");
-        display_area.appendChild(new_area);
-        // element add functions 
-        display_area.appendChild();
-    }
-
-
-    // button functions below 
+    const JsonMain = {};
+    function JsonConstruct() {
+        return {"Topic":{
+            "topic-name" : "gg",
+            "curio-list" : [{"curio":
+    const cancelButton = document.getElementById {"name": "Meaning of life", "priority": "urgent", "date": something}}]}};
+    };
+("cancel-button");
+    const confirmButton = document.getElementById("confirm-button");
 
     addButton.addEventListener("click", () => {
         dialog.showModal();
     });
-
-    const cancelButton = document.getElementById("cancel-button");
-    const confirmButton = document.getElementById("confirm-button");
 
     confirmButton.addEventListener("click",() =>{
         //add append data to json a function
         Clearvalues();
         
         Object.setPrototypeOf(AddBook,New_book);
-        confirm_books = new AddBook();
+        confirm_books = new NewTopic();
         Display_books();
         console.log(all_books);
     });
@@ -52,7 +34,24 @@ document.addEventListener("DOMContentLoaded", () => {
         Clearvalues();
         dialog.close();
     });
+
+    function TopicAddToJson(){{
+        contents
+    }};
+
     function Clearvalues() {
+        topic.value = '';
+    };
+
+    function TopicDisplay(){
+        new_area = document.createElement("div");
+        new_area.classList.add("new_area");
+        display_area.appendChild(new_area);
+        // element add functions 
+    }
+
+    function CurioAdd(){
 
     };
+
 });
