daae2009846899da1273e51d00130054eadb8ce6
Khantdotcom <kokhanthtay@gmail.com>
2024-09-27 23:01:27 +0630

    Added a few styling

--- Curio To-know-list App/src/index.js
+++ Curio To-know-list App/src/index.js
@@ -1,5 +1,7 @@
+import "./styles.css";
 document.addEventListener("DOMContentLoaded", () => {
     //add diplay
+
     const main_json = {};
     const display_area = document.getElementById("display-area");
     const addButton = document.querySelector(".add-button");
@@ -31,11 +33,12 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     function TopicDisplay(){
-        new_area = document.createElement("div");
+        const new_area = document.createElement("div");
         new_area.classList.add("new_area");
         display_area.appendChild(new_area);
         new_area.textContent = topic.value;
         // element add functions 
+        CurioArea_create(new_area);
     }
 
     //data structure
@@ -44,10 +47,20 @@ document.addEventListener("DOMContentLoaded", () => {
         main_json[topic.value] = {};
     }};
 
-    function CurioAdd(){
-
+    function CurioArea_create(locaiton){
+        const curio_div = document.createElement("div");
+        curio_div.classList.add("curio_div")
+        new_curio.classList.add("curio");
+        curio_div.appendChild(new_curio);
+        locaiton.appendChild(curio_div);
+    };
+    function Curio_add(curioitem_name){
+        const curio_input = document.createElement("input");
+        curio_input.type = "checklist";
+        curio_input.textContent= `${curioitem_name}`;
     };
 
+    
 
 
 });
