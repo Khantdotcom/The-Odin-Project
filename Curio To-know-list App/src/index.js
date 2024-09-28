54027413220ed00115998371d3c6f4c3f9ca4fef
Khantdotcom <kokhanthtay@gmail.com>
2024-09-28 17:46:48 +0630

    Moving on to next modules although the app is not finished

--- Curio To-know-list App/src/index.js
+++ Curio To-know-list App/src/index.js
@@ -6,20 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const display_area = document.getElementById("display-area");
     const addButton = document.querySelector(".add-button");
 
-    addButton.addEventListener("click", () => {
-        dialog.showModal();
-    });
     //dialog Pop-up
     const dialog = document.querySelector("dialog");
     const topic = document.getElementById("topic-name");
     const cancelButton = document.getElementById("cancel-button");
     const confirmButton = document.getElementById("confirm-button");
 
-    confirmButton.addEventListener("click",() =>{
+    addButton.addEventListener("click", () => {
+        dialog.showModal();
+    });
+
+    confirmButton.addEventListener("click",(event) =>{
         //add append data to json a function
+        event.preventDefault();
         TopicAddToJson();
         TopicDisplay();
         Clearvalues();
+
         dialog.close();
     });
 
@@ -37,40 +40,22 @@ document.addEventListener("DOMContentLoaded", () => {
         new_area.classList.add("new_area");
         display_area.appendChild(new_area);
         new_area.textContent = topic.value;
-        // element add functions 
-        CurioArea_create(new_area);
+
+        const curio_div = document.createElement("div");
+        curio_div.classList.add("curio_div");
+            new_area.appendChild(curio_div);
+
+        curio_div.addEventListener("click", () =>{
+            const curio_input = document.createElement("input");
+            curio_input.type = "checkbox";
+            curio_input.name = "hahah";
+            new_area.appendChild(curio_input);
+        });
     }
 
     //data structure
 
-    function TopicAddToJson(){{
+    function TopicAddToJson(){
         main_json[topic.value] = {};
-    }};
-
-    function CurioArea_create(locaiton){
-        const curio_div = document.createElement("div");
-        curio_div.classList.add("curio_div");
-        locaiton.appendChild(curio_div);
-
-        const curio_button = document.createElement("button");
-        curio_button.addEventListener("click", () =>{
-            Curio_add(locaiton);
-        });
-        curio_div.appendChild(curio_button);
-    };
-
-    function New_dialog(locaiton) {
-        const new_dialog = document.createElement("dialog");
-        const curio_dialog_input = document.createElement("input");
-        curio_dialog_input.type = "text";
-        new_dialog.appendChild(curio_dialog_input);
-        location.appendChild(new_dialog);
-    };
-
-    function Curio_add(locaiton){
-        const curio_input = document.createElement("input");
-        curio_input.type = "checkbox";
-        curio_input.textContent= `${curioitem_name}`;
-        locaiton.appendChild(curio_input);
     };
 });
