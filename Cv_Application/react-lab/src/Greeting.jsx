5bb589a3befc03daf484179ad6bdc8155fa0c5c6
Khant Htay <kokhanthtay@gmail.com>
2024-11-09 12:13:55 +0630

    Cv apply app started

--- Cv_Application/react-lab/src/Greeting.jsx
+++ Cv_Application/react-lab/src/Greeting.jsx
@@ -3,3 +3,33 @@ function Greeting(){
 }
 
 export default Greeting
+
+function Profile({person}){
+    return(
+    <img
+            className="profile-img"
+            alt={person.name}
+            width={100}
+            height={100}
+            src="The-Odin-Project\Cv_Application\react-lab\public\vite.svg"
+        />);
+}
+
+function Comment({owner,context}){
+    return(
+        <>
+        <Profile />
+        <p>{context}</p>
+        </>
+    );
+}
+
+function Item({ispacked, name}){
+    return(
+        <li>
+            {ispacked? name +'gg':name}
+        </li>
+    );}
+
+//props, conditional , mapping list// state
+
