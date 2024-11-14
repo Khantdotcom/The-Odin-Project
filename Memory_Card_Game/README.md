79582fdb639f858546a1dd6adfcf23cf70943dd3
Khant Htay <kokhanthtay@gmail.com>
2024-11-14 11:49:29 +0630

    New_built : overall game logic

--- Memory_Card_Game/README.md
+++ Memory_Card_Game/README.md
@@ -2,6 +2,6 @@
 2. card design > 
 - API info fetch 
     - how many cards >> choose
-- random display
+- random_display()
 3. > onCLick function(check its click/state >>
-4. game_end(), next_level()>> random again)
+4. game_end(), next_level()>> random_display())
---
+++ Memory_Card_Game/react-lab/src/component.jsx
@@ -0,0 +1,54 @@
+const fetched_data = [{image:"https",description:'sex',title:'loe'}]
+
+function info_fetch(number_of_cards) {
+    fetched_data.append()
+}
+
+function make_input(game_type){
+    return (
+        <input
+        type="choice"
+        name= "se"
+        required>
+            
+        </input>
+    )
+}
+
+//card_component_div with its conditions
+function each_card(){
+    <>
+    <div onclick="game_end()">
+        <img></img>
+        <h3></h3>
+        <p></p>
+    </div>
+    </>
+    };
+
+
+// doesn't have to run the loop(info fetch and display) again every time re render 
+function random_display(number){
+    each_card
+}
+
+game_end(
+    if state of clicked card == 1 >>  
+    end_display(lose)
+
+    elif{ score === number of cards} >> end_display(win)
+    end_display(lose)
+
+    else
+    n => n + 1
+    random_display()
+)
+
+best_score_check(
+    compare current > state score 
+    set score
+)
+
+end_display (condition){
+    console.log('You {condition}')
+}
