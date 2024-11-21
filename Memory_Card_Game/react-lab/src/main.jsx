360d80aea4211218006ac54f87a3f9cfc37bd795
Khant Htay <kokhanthtay@gmail.com>
2024-11-21 17:24:31 +0630

    Testing Heading

--- Memory_Card_Game/react-lab/src/main.jsx
+++ Memory_Card_Game/react-lab/src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { render, screen } from '@testing-library/react';
+import { expect } from 'vitest';
+import { JSDOM } from 'jsdom';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App/>);
+    expect(screen.getByRole("heading").textContent).toMatch(/Test/i);
+  });
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
